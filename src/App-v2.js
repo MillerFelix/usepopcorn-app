cc16a5892ddd7505f64fbef54fe7de82a5dfcb85
MillerFelix <millerredigolo@gmail.com>
2024-11-04 16:37:55 -0300

    Developing local storage of the films

--- src/App-v2.js
+++ src/App-v2.js
@@ -9,13 +9,10 @@ const KEY = "b4224635";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
-  const [watched, setWatched] = useState(function () {
-    const storedValue = localStorage.getItem("watched");
-    return JSON.parse(storedValue);
-  });
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -32,13 +29,6 @@ export default function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
-
-  useEffect(
-    function () {
-      localStorage.setItem("watched", JSON.stringify(watched));
-    },
-    [watched]
-  );
 
   useEffect(
     function () {
@@ -62,6 +52,7 @@ export default function App() {
           setError("");
         } catch (err) {
           if (error.name !== "AbortError") {
+            console.log(err.message);
             setError(err.message);
           }
         } finally {
